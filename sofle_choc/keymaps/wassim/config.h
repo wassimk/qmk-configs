82a38de6518b0807c39742aeab5431fd8bfe42a3
Wassim Metallaoui <wassim@metallaoui.com>
2024-01-23 19:15:37 -0600

    fix: keyboard non-responsive after power cycle
    This fixes the issue where the keyboard doesn't come back after a power
    cycle of the computer (Apple Studio Display) it's attached to.

--- sofle_choc/keymaps/wassim/config.h
+++ sofle_choc/keymaps/wassim/config.h
@@ -19,15 +19,6 @@
 // see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 #define MASTER_RIGHT
 
-// Enabling this option changes the startup behavior to listen for an
-// active USB communication to delegate which part is master and which
-// is slave. With this option enabled and theres’s USB communication,
-// then that half assumes it is the master, otherwise it assumes it
-// is the slave.
-//
-// I've found this helps with some ProMicros where the slave does not boot
-#define SPLIT_USB_DETECT
-
 #ifdef RGB_MATRIX_ENABLE
 #define RGB_DISABLE_WHEN_USB_SUSPENDED     // turn off effects when suspended
 #define SPLIT_TRANSPORT_MIRROR             // If LED_MATRIX_KEYPRESSES or LED_MATRIX_KEYRELEASES is enabled, you also will want to enable SPLIT_TRANSPORT_MIRROR
